c8d018ae96e2d073c48f68c731d2588beaa6cbf0
vladgibeskul <wgv96300@gmail.com>
2024-10-29 19:47:39 +0200

    Практикум 4

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Задача 1
+            //Console.WriteLine("Введите первое число:");
+            //double number1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите второе число:");
+            //double number2 = Convert.ToDouble(Console.ReadLine());
+
+            //double sumTwo = number1 + number2;
+            //Console.WriteLine($"Сумма двух чисел: {sumTwo}");
+
+            //Console.WriteLine("Введите третье число:");
+            //double number3 = Convert.ToDouble(Console.ReadLine());
+
+            //double sumThree = sumTwo + number3;
+            //Console.WriteLine($"Сумма трех чисел: {sumThree}");
+
+
+
+            // Задача 2. Целые числа
+            //Console.WriteLine("Введите длину первой стороны (целое число):");
+            //int side1Int = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine("Введите длину второй стороны (целое число):");
+            //int side2Int = Convert.ToInt32(Console.ReadLine());
+
+            //int perimeterInt = 2 * (side1Int + side2Int);
+            //int areaInt = side1Int * side2Int;
+
+            //Console.WriteLine($"Периметр (целые числа): {perimeterInt}");
+            //Console.WriteLine($"Площадь (целые числа): {areaInt}");
+
+
+            // Вещественные числа
+            //Console.WriteLine("nВведите длину первой стороны (вещественное число):");
+            //double side1Double = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите длину второй стороны (вещественное число):");
+            //double side2Double = Convert.ToDouble(Console.ReadLine());
+
+            //double perimeterDouble = 2 * (side1Double + side2Double);
+            //double areaDouble = side1Double * side2Double;
+
+            //Console.WriteLine($"Периметр (вещественные числа): {perimeterDouble}");
+            //Console.WriteLine($"Площадь (вещественные числа): {areaDouble}");
+
+
+            //Задача 3. 
+            //Console.WriteLine("Введите длину первого катета (вещественное число):");
+            //double cathetus1 = Convert.ToDouble(Console.ReadLine());
+
+            //Console.WriteLine("Введите длину второго катета (вещественное число):");
+            //double cathetus2 = Convert.ToDouble(Console.ReadLine());
+
+            //double hypotenuse = Math.Sqrt(cathetus1 * cathetus1 + cathetus2 * cathetus2);
+
+            //Console.WriteLine($"Гипотенуза треугольника: {hypotenuse}");
+
+
+            //Задача 4. 
+            //Console.WriteLine("Введите длину стороны квадрата (целое число):");
+            //int sideInt = Convert.ToInt32(Console.ReadLine());
+
+            //int perimeterInt = 4 * sideInt;
+            //int areaInt = sideInt * sideInt;
+
+            //Console.WriteLine($"Периметр квадрата (целый тип): {perimeterInt}");
+            //Console.WriteLine($"Площадь квадрата (целый тип): {areaInt}");
+
+            //Console.WriteLine("Введите длину стороны квадрата (вещественное число):");
+            //double sideDouble = Convert.ToDouble(Console.ReadLine());
+
+            //double perimeterDouble = 4 * sideDouble;
+            //double areaDouble = sideDouble * sideDouble;
+
+            //Console.WriteLine($"Периметр квадрата (вещественный тип): {perimeterDouble}");
+            //Console.WriteLine($"Площадь квадрата (вещественный тип): {areaDouble}");
+        }
+    }
+}
