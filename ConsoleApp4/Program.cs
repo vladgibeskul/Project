cb04bc8657da84261ee0b4e2b535e385d0a44eaa
vladgibeskul <wgv96300@gmail.com>
2024-11-17 20:38:16 +0200

    zadanie1

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hi();
+            By();
+            Console.ReadLine();
+                
+        }
+        public static void Hi()
+        {
+            Console.WriteLine("Добро пожаловать!");
+        }
+        public static void By()
+        {
+            Console.Write("До свидания");
+        }
+    }
+}
