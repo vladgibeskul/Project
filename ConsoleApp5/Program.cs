bbd12a30b455dde119652cf2e125dd424b0d5920
vladgibeskul <wgv96300@gmail.com>
2024-11-17 20:46:34 +0200

    zadanie2

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ввод х=");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Ввод y=");
+            int y = int.Parse(Console.ReadLine());
+            Console.Write($"Cумма={Sum(x, y)}");
+            Console.ReadLine();
+        }
+        public static int Sum(int x, int y)
+        {
+            return x + y;
+        }
+    }
+}
