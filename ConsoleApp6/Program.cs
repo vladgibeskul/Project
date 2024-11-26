12d2caeb7d2f0cf3178bf5a31cd724b54ff36a88
vladgibeskul <wgv96300@gmail.com>
2024-11-26 22:43:52 +0200

    10_practicum

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////task 1
+
+            //public static void Sum()
+            //{
+            //    Console.Write($"  x={Math.Round(F(5) + F(12) + F(19))}");
+            //    Console.ReadKey();
+            //}/// <summary>
+            ///// ФУНКЦИЯ
+            ///// </summary>
+            ///// <param name="x"></param>
+            ///// <returns></returns>
+            //public static double F(int x)
+            //{
+            //    return (Math.Sqrt(x) + x) / 2;
+            //}
+            //static void Main(string[] args)
+            //{
+            //    Sum();
+            //}
+            ///// <summary>
+            ///// вывод
+            ///// </summary>
+
+
+
+            ////task2
+
+
+            //public static void Sum()
+            //{
+            //    Console.Write($"  x={Math.Round(F(5, 7) + F(12, 8) + F(19, 2))}");
+            //    Console.ReadKey();
+
+            //}/// <summary>
+            // /// ФУНКЦИЯ
+            // /// </summary>
+            // /// <param name="a"></param>
+            // /// <param name="b"></param>
+            // /// <returns></returns>
+            //public static double F(int a, int b)
+            //{
+            //    return (Math.Sqrt(a) + a) / (Math.Sqrt(b) + b);
+            //}
+            //static void Main(string[] args)
+            //{
+
+            //    Sum();
+            //}
+
+            //task3
+
+
+            //public static int Input(string message)
+            //{
+            //    Console.Write(message);
+            //    return int.Parse(Console.ReadLine());
+            //}
+            //public static double Func(int x1, int x2)
+            //{
+            //    return x1 > x2 ? x1 : x2;
+            //}
+            //static void Value()
+            //{
+            //    int x1 = Input("x1= ");
+            //    int x2 = Input("x2= ");
+            //    Console.WriteLine($"самое большое число = {Func(x1, x2)}");
+            //    Console.ReadKey();
+            //}
+            //static void Main(string[] args)
+            //{
+            //    Value();
+            //}
+
+
+            //task4
+            //static int Max1(int x1, int x2, int y1, int max)
+            //{
+            //    return max = new[] { x1, x2, y1 }.Max();
+            //}
+
+            //public static int Input(string message)
+            //{
+            //       Console.Write(message);
+            //       return int.Parse(Console.ReadLine());
+            //}
+            //static void Value()
+            //{
+            //    int max = 0;
+            //    int y1 = Input("y1= ");
+            //    int x1 = Input("x1= ");
+            //    int x2 = Input("x2= ");
+            //    Console.WriteLine($"самое большое число = {Max1(x1, x2, y1, max)}");
+            //    Console.ReadKey();
+
+            //}
+            //static void Main(string[] args)
+            //{
+            //    Value();
+            //}
+
+
+
+            //task5
+            //static int Max1(int a, int b,int c,int d)
+            //{
+            //     a = new[] { a, b }.Max();
+
+            //    c = new[] { c, d }.Max();
+            //    return a + c;
+            //}
+
+
+
+            //public static int Input(string message)  
+            //{
+            //    Console.Write(message);
+            //    return int.Parse(Console.ReadLine());
+
+            //}
+
+            //static void Value()
+            //{
+            //    int a = Input("a= ");
+            //    int b = Input("b= ");
+            //    int c = Input("c= ");
+            //    int d = Input("d= ");
+            //    Console.WriteLine($"s = {Max1(a,b,c,d)}");
+            //    Console.ReadKey();
+            //}
+            //static void Main(string[] args)
+            //{
+            //    Value();
+            //}
+        }
+    }
+}
